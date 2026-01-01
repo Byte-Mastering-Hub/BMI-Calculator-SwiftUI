09cf368cdc52498c0ca5ef941ba381e55b8497a3
Byte-Mastering-Hub <sumit1919ss@gmail.com>
2026-01-02 00:09:55 +0530

    Initial Commit

---
+++ Body Mass Index/Body_Mass_IndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Body_Mass_IndexApp.swift
+//  Body Mass Index
+//
+//  Created by Sumit on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Body_Mass_IndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
